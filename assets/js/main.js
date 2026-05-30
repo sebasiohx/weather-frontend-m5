4d98453d4aebfd155d33b03fd40d1bf08b55b810
sebasiohx <seba.olate.h@gmail.com>
2026-05-30 02:31:44 -0400

    feat: integracion cards regionales, vista Home completada

--- assets/js/main.js
+++ assets/js/main.js
@@ -61,7 +61,7 @@ function renderizarPronostico(pronostico) {
 
   const listaElementosPronostico = pronostico.fechas.map((fecha, index) => {
     const fechaDiaActual = new Date(fecha);
-    const numeroDia = fechaDiaActual.getDate(); // 29
+    const numeroDia = anteponerCero(fechaDiaActual.getDate());
     const dias = ["dom", "lun", "mar", "miér", "juev", "vier", "sáb"];
     const diaSemana = dias[fechaDiaActual.getDay()];
 
@@ -205,4 +205,125 @@ async function renderizarHero() {
   divCardPronostico.append(ulListaPronostico);
 }
 
+// Funcion para renderizar una card
+function crearCard(lugar) {
+  const col = document.createElement("div");
+  col.className = "col-12 col-sm-6 col-md-4 col-lg-3";
+  //=================================================
+  const article = document.createElement("article");
+  article.className = "card regions-card";
+  article.dataset.id = lugar.id;
+  //=================================================
+  const figureCardImage = document.createElement("figure");
+  figureCardImage.className = "regions-card_image-container";
+  const img = document.createElement("img");
+  img.src = `./assets/img/${lugar.img}`;
+  img.className = "regions-card__image object-fit-cover";
+  img.alt = lugar.nombreCiudad;
+  const figcaption = document.createElement("figcaption");
+  figcaption.className = "regions-card__caption";
+  const spanRegion = document.createElement("span");
+  spanRegion.className = "regions-card__region-title";
+  spanRegion.textContent = lugar.nombreRegion;
+  figcaption.append(spanRegion);
+  figureCardImage.append(img, figcaption);
+  //=================================================
+  const cardBody = document.createElement("div");
+  cardBody.className = "card-body";
+  //=================================================
+  const h3 = document.createElement("h3");
+  h3.className = "card-title h5";
+  //=================================================
+  const iconoUbicacion = document.createElement("i");
+  iconoUbicacion.className = "fa-solid fa-location-dot tc-primary";
+  //=================================================
+  const nombreCiudad = document.createTextNode(`${lugar.nombreCiudad}`);
+
+  h3.append(iconoUbicacion);
+  h3.append(nombreCiudad);
+  //=================================================
+  const h2 = document.createElement("h2");
+  h2.className =
+    "regions-card__current-climate display-5 text-body-emphasis mb-2";
+  //=================================================
+  const spanTemperatura = document.createElement("span");
+  spanTemperatura.className = "";
+  spanTemperatura.textContent = `${anteponerCero(lugar.climaActual.tempActual)}`;
+  const spanUnidadTemperatura = document.createElement("span");
+  spanUnidadTemperatura.className = "regions-card__unit";
+  spanUnidadTemperatura.textContent = lugar.unidadMedida;
+  const divTempContainer = document.createElement("div");
+  divTempContainer.className = "tc-accent d-flex";
+  divTempContainer.append(spanTemperatura, spanUnidadTemperatura);
+  //=================================================
+  const iconoClima = document.createElement("i");
+  iconoClima.className = `fa-solid ${traducirClima(lugar.climaActual.clima, true).icono} display-6 tc-primary mt-3`;
+
+  h2.append(divTempContainer, iconoClima);
+  //=================================================
+  const p = document.createElement("p");
+  p.className = "regions-card__min-max ms-1 mb-0";
+  //=================================================
+  const spanRango = document.createElement("span");
+  spanRango.textContent = `${anteponerCero(lugar.pronosticoSemanal.tempMinima)}°/${anteponerCero(lugar.pronosticoSemanal.tempMaxima)}°`;
+  //=================================================
+  const spanEstado = document.createElement("span");
+  spanEstado.className = "badge bgc-accent";
+  spanEstado.textContent = traducirClima(lugar.climaActual.clima, true).titulo;
+
+  p.append(spanRango);
+  p.append(spanEstado);
+  //=================================================
+  // Ensamblar todo
+  cardBody.append(h3);
+  cardBody.append(h2);
+  cardBody.append(p);
+
+  article.append(figureCardImage);
+  article.append(cardBody);
+
+  col.append(article);
+
+  return col;
+}
+
+// Funcion para renderizar todas las cards
+async function rendeizarListaCards(listaLugares) {
+  const divContainer = document.createElement("div");
+  divContainer.className = "d-flex justify-content-center";
+  const divSpinner = document.createElement("div");
+  divSpinner.className = "spinner-border tc-primary border-5";
+  divSpinner.role = "status";
+  divSpinner.style.width = "4.5rem";
+  divSpinner.style.height = "4.5rem";
+  const spanLoading = document.createElement("span");
+  spanLoading.className = "visually-hidden";
+  spanLoading.textContent = "Cargando...";
+
+  divSpinner.append(spanLoading);
+  divContainer.append(divSpinner);
+
+  regionesContainer.append(divContainer);
+
+  const lugares = await listaLugares;
+  regionesContainer.textContent = "";
+
+  for (const lugar of lugares) {
+    const card = crearCard(lugar);
+    regionesContainer.append(card);
+  }
+}
+
+/* Eventos */
+regionesContainer.addEventListener("click", (e) => {
+  const card = e.target.closest(".regions-card");
+  // guardian
+  if (!card) return;
+  const idRegion = +card.dataset.id;
+  //mostrarDetalle(idRegion);
+  console.log(idRegion);
+});
+
+/* Ejecución */
 renderizarHero();
+rendeizarListaCards(climaChileApp.cargarLugares());
